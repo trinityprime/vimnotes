64d9b3e4d17569f2a9586f9e74082014d8b0a184
Ryan <limkl.ryan@gmail.com>
2024-07-21 23:44:44 +0800

    merge sort, quick sort qns

--- Y2S1/DSA/adv_sort.py
+++ Y2S1/DSA/adv_sort.py
@@ -52,7 +52,7 @@ Now, combine both
 
 Practice Questions:
 -------------------
-1. Sort in ascending order
+1. Sort in ascending order via quick sort
 
 *4* 7 1 8 3 2 6 5
 
@@ -69,7 +69,7 @@ Practice Questions:
 1 2 3 4 5 6 7 8
 
 
-2. Sort in descending order
+2. Sort in descending order via quick sort
 
 *5* 2 7 8 1 4 6 3
 
@@ -85,6 +85,99 @@ Practice Questions:
 8 7 6     4 3 2 1 
 
 8 7 6 5 4 3 2 1 
+
+
+3. Sort in ascending order via merge sort 
+
+38 27 43 3 9 82 10
+
+38 27 | 43 3 | 9 82 | 10
+
+Part 1: 38 | 27  43 | 3  Part 2: 9 | 82   10
+
+Part 1:
+
+38 | 27 --> 27 38
+
+43 | 3 --> 3 43
+
+38 27 | 43 3 -->  3 27 38 43
+
+Part 2:
+
+9 | 82 --> 9 82
+
+10 --> 10
+
+9 | 82 10 --> 9 10 82
+
+New:
+
+3 9 10 27 38 43 82
+
+
+4. Sort in descending order via merge sort
+
+15 8 24 42 7 19 31 5
+
+15 8 24 42 | 7 19 31 5
+
+Part 1: 15 8 | 24 42  Part 2: 7 19 | 31 5
+
+Part 1: 
+
+15 | 8 --> 15 8 
+
+24 | 42 --> 42 24
+
+15 8 | 24 42 --> 42 24 15 8
+
+Part 2:
+
+7 | 19 --> 19 7
+
+31 | 5 --> 31 5
+
+7 19 | 31 5 --> 31 19 7 5
+
+New:
+
+42 31 24 19 15 8 7 5 
+
+
+5. Sort in ascending order via quick sort
+
+15 8 24 42 7 19 31 5
+
+8 7 5 *15* 24 42 19 31
+
+7 5 *8*   19 *24* 42 31
+
+*7* 5     *19* 42 31
+
+5         31 *42*
+          
+          21
+
+5 7 8    19 21 31 42
+
+5 7 8 15 19 21 31 42 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
